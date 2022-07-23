7c99533f983b25bb0a8051c4ca67a89e5f813387
monu <monujoshi471@gmail.com>
2022-07-23 17:09:50 +0530

    this is my code push

---
+++ bin1.py
@@ -0,0 +1,7 @@
+a = 141
+l = []
+i = 2
+while i < a:
+      k = a%2
+      l.append(k)
+print(l)
---
+++ mongo.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://monu:monu471@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name": "sudhanshu",
+    "email": "sudhanshu@ineuron.ai",
+    "surname": "kumar"
+}
+db1 = client['mongo']
+coll = db1['test']
+coll.insert_one(d)
+
+
